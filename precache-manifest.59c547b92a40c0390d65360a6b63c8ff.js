1549048ad46f9c39628b4be9dc0b71d2596c99bb
Travis <travic@travis-ci.org>
2019-10-16 17:17:35 +0000

    Travis build 82

---
+++ precache-manifest.59c547b92a40c0390d65360a6b63c8ff.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aff6bbcbb3274c27d30d279edf379be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c48d1f5e47ac39c67bf1",
+    "url": "/static/css/main.95dc1e1f.chunk.css"
+  },
+  {
+    "revision": "47e3875467899e5d0afb",
+    "url": "/static/js/2.7c98ca3d.chunk.js"
+  },
+  {
+    "revision": "c48d1f5e47ac39c67bf1",
+    "url": "/static/js/main.57ff3b4d.chunk.js"
+  },
+  {
+    "revision": "3ae9dbb2eb3ede2de076",
+    "url": "/static/js/runtime-main.b3a942fc.js"
+  },
+  {
+    "revision": "6b7d051922e55e5cd0cc2784d5a56574",
+    "url": "/static/media/bg_jumbotron.6b7d0519.jpg"
+  },
+  {
+    "revision": "3b49bd07032b1b4f57514cd38178c6dc",
+    "url": "/static/media/dataforeningen.3b49bd07.svg"
+  },
+  {
+    "revision": "46bb59058ba39a53b02a082fd30eb897",
+    "url": "/static/media/email.46bb5905.svg"
+  },
+  {
+    "revision": "34e73505be53a83b3d32236dab04f124",
+    "url": "/static/media/facebook.34e73505.svg"
+  },
+  {
+    "revision": "01b7ea06a552892f2fe49d9a5738e574",
+    "url": "/static/media/index_01.01b7ea06.jpg"
+  },
+  {
+    "revision": "b303a4af7375a9a7455fa0cf5c0d090f",
+    "url": "/static/media/index_03.b303a4af.jpg"
+  },
+  {
+    "revision": "9cfb42522b639abc29d0fe8a73d75677",
+    "url": "/static/media/ixda.9cfb4252.svg"
+  },
+  {
+    "revision": "9e6545778ebefe3a8139d25846acfef2",
+    "url": "/static/media/javabin.9e654577.svg"
+  },
+  {
+    "revision": "d653670ce28b79d26b200564625a7205",
+    "url": "/static/media/nnug.d653670c.svg"
+  },
+  {
+    "revision": "cac1f2191e5f96b01fa35953c221c1b1",
+    "url": "/static/media/spillmakerlauget.cac1f219.svg"
+  },
+  {
+    "revision": "4399c36b3f777c560f178fba62a0a183",
+    "url": "/static/media/tdc_jumbotron.4399c36b.png"
+  },
+  {
+    "revision": "a1223de504180a12574a17a1f30c5a08",
+    "url": "/static/media/twitter.a1223de5.svg"
+  }
+]);
